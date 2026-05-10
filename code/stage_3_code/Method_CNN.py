dbeec1c6f6bd494b04a96cf81162a98d47d2c910
yicsi <yicsi@ucdavis.edu>
2026-05-09 21:56:19 -0700

    Add stage 3 CNN code

--- code/stage_3_code/Method_CNN.py
+++ code/stage_3_code/Method_CNN.py
@@ -1,12 +1,9 @@
 '''
-Concrete MethodModule class for a specific learning MethodModule
+Concrete MethodModule class for CNN
 '''
 
-# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
-# License: TBD
-
 from code.base_class.method import method
-from code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
+from code.stage_3_code.Evaluate_Accuracy import Evaluate_Accuracy
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
@@ -15,11 +12,11 @@ import matplotlib.pyplot as plt
 import os
 
 
-class Method_MLP(method, nn.Module):
+class Method_CNN(method, nn.Module):
     data = None
-    max_epoch = 20
+    max_epoch = 10
     learning_rate = 1e-3
-    batch_size = 256
+    batch_size = 64
 
     def __init__(self, mName, mDescription):
         method.__init__(self, mName, mDescription)
@@ -27,20 +24,34 @@ class Method_MLP(method, nn.Module):
 
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        self.fc_layer_1 = nn.Linear(784, 512)
-        self.activation_func_1 = nn.ReLU()
+        # CNN for MNIST: input shape = 1 x 28 x 28
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2, 2)
 
-        self.fc_layer_2 = nn.Linear(512, 256)
-        self.activation_func_2 = nn.ReLU()
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2, 2)
 
-        self.fc_layer_3 = nn.Linear(256, 10)
+        # After two pooling layers: 28 -> 14 -> 7
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.relu3 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 10)
 
         self.to(self.device)
 
     def forward(self, x):
-        h = self.activation_func_1(self.fc_layer_1(x))
-        h = self.activation_func_2(self.fc_layer_2(h))
-        y_pred = self.fc_layer_3(h)
+        # Convert flat MNIST input to image format
+        x = x.reshape(-1, 1, 28, 28)
+
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+
+        x = x.view(x.size(0), -1)
+
+        x = self.relu3(self.fc1(x))
+        y_pred = self.fc2(x)
+
         return y_pred
 
     def evaluate_dataset(self, X_tensor, y_true, loss_function, evaluator_name):
@@ -56,6 +67,7 @@ class Method_MLP(method, nn.Module):
             'true_y': y_true,
             'pred_y': pred_labels
         }
+
         acc = accuracy_evaluator.evaluate()
         return loss, acc
 
@@ -69,7 +81,11 @@ class Method_MLP(method, nn.Module):
         test_y_true = torch.LongTensor(np.array(test_y))
 
         train_dataset = TensorDataset(train_X_tensor, train_y_true)
-        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True
+        )
 
         epoch_list = []
         train_loss_list = []
@@ -95,33 +111,48 @@ class Method_MLP(method, nn.Module):
                 epoch_loss += train_loss.item() * batch_X.size(0)
 
             avg_epoch_loss = epoch_loss / len(train_dataset)
+
             train_eval_loss, train_acc = self.evaluate_dataset(
-                train_X_tensor, train_y_true, loss_function, 'training evaluator'
+                train_X_tensor,
+                train_y_true,
+                loss_function,
+                'training evaluator'
             )
+
             test_loss, test_acc = self.evaluate_dataset(
-                test_X_tensor, test_y_true, loss_function, 'testing evaluator'
+                test_X_tensor,
+                test_y_true,
+                loss_function,
+                'testing evaluator'
             )
 
             epoch_list.append(epoch)
-            train_loss_list.append(avg_epoch_loss if epoch == 0 else train_eval_loss)
+            train_loss_list.append(avg_epoch_loss)
             train_acc_list.append(train_acc)
             test_loss_list.append(test_loss)
             test_acc_list.append(test_acc)
 
+            print(
+                'Epoch:', epoch,
+                'Train Accuracy:', train_acc,
+                'Train Loss:', train_loss_list[-1],
+                'Test Accuracy:', test_acc,
+                'Test Loss:', test_loss
+            )
 
-            print('Epoch:', epoch,
-                    'Train Accuracy:', train_acc,
-                    'Train Loss:', train_loss_list[-1],
-                    'Test Accuracy:', test_acc,
-                    'Test Loss:', test_loss)
+        self.save_learning_curves(
+            epoch_list,
+            train_loss_list,
+            train_acc_list,
+            test_loss_list,
+            test_acc_list
+        )
 
-        self.save_learning_curves(epoch_list, train_loss_list, train_acc_list,
-                                  test_loss_list, test_acc_list)
         print('Final Train Loss:', train_loss_list[-1])
         print('Final Test Loss:', test_loss_list[-1])
 
     def save_learning_curves(self, epochs, train_losses, train_accs, test_losses, test_accs):
-        save_folder = '../../result/stage_2_result/'
+        save_folder = '../../result/stage_3_result/'
         os.makedirs(save_folder, exist_ok=True)
 
         plt.figure()
@@ -129,9 +160,9 @@ class Method_MLP(method, nn.Module):
         plt.plot(epochs, test_losses, label='Test Loss')
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
-        plt.title('Train vs Test Loss Curve')
+        plt.title('CNN Train vs Test Loss Curve')
         plt.legend()
-        plt.savefig(save_folder + 'training_loss_curve.png')
+        plt.savefig(save_folder + 'cnn_training_loss_curve.png')
         plt.close()
 
         plt.figure()
@@ -139,25 +170,35 @@ class Method_MLP(method, nn.Module):
         plt.plot(epochs, test_accs, label='Test Accuracy')
         plt.xlabel('Epoch')
         plt.ylabel('Accuracy')
-        plt.title('Train vs Test Accuracy Curve')
+        plt.title('CNN Train vs Test Accuracy Curve')
         plt.legend()
-        plt.savefig(save_folder + 'training_accuracy_curve.png')
+        plt.savefig(save_folder + 'cnn_training_accuracy_curve.png')
         plt.close()
 
     def test(self, X):
         super().train(False)
+
         with torch.no_grad():
             X_tensor = torch.FloatTensor(np.array(X)).to(self.device)
             y_pred = self.forward(X_tensor)
+
         return y_pred.max(1)[1].cpu()
 
     def run(self):
         print('method running...')
         print('--start training...')
+
         self.train(
-            self.data['train']['X'], self.data['train']['y'],
-            self.data['test']['X'], self.data['test']['y']
+            self.data['train']['X'],
+            self.data['train']['y'],
+            self.data['test']['X'],
+            self.data['test']['y']
         )
+
         print('--start testing...')
         pred_y = self.test(self.data['test']['X'])
-        return {'pred_y': pred_y, 'true_y': self.data['test']['y']}
+
+        return {
+            'pred_y': pred_y,
+            'true_y': self.data['test']['y']
+        }
